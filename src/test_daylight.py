87b16fa327af3de6927ef24ea601edc94db267d0
Knut Alseth <knut@alseth.no>
2026-09-19 16:28:12 +0200

    legg til test_daylight.py

---
+++ src/test_daylight.py
@@ -0,0 +1,23 @@
+import daylight
+import datetime as dt
+
+def test_daylight_all_year():
+    year = 2026
+    current = dt.date(year, 1, 1)
+    one_day = dt.timedelta(days=1)
+    while current.year == year:
+        if current.month != 5:
+            current += one_day
+            continue
+        # print(current)
+
+        act_date_time = dt.datetime.combine(
+            current,
+            dt.time(12, 0),
+            tzinfo=dt.UTC,
+        )       
+        print(current, daylight.is_light_enough(act_date_time=act_date_time))
+        current += one_day
+        time.sleep(1)
+
+test_daylight_all_year()
