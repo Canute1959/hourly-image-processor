8d5b6f420a7227684117963e234c26039abe1558
Knut Alseth <knut@alseth.no>
2026-09-22 08:43:10 +0200

    Opprett camera.py og oppdater main.py

---
+++ src/hourly_image_processor/camera.py
@@ -0,0 +1,37 @@
+# src/hourly_image_processor/camera.py
+
+import datetime as dt
+import subprocess
+from pathlib import Path
+
+
+def take_picture(
+    root_directory="/home/knut/Pictures/timelapse",
+    width=1920,
+    height=1080,
+) -> Path:
+
+    now = dt.datetime.now()
+
+    year = now.strftime("%Y")
+    month = now.strftime("%m")
+    day = now.strftime("%d")
+    hour = now.strftime("%H")
+
+    directory = Path(root_directory) / year / month / day
+    directory.mkdir(parents=True, exist_ok=True)
+
+    filename = directory / f"img{year}{month}{day}-{hour}.jpg"
+
+    subprocess.run(
+        [
+            "rpicam-still",
+            "--output", str(filename),
+            "--width", str(width),
+            "--height", str(height),
+            "--nopreview",
+        ],
+        check=True,
+    )
+
+    return filename
--- src/hourly_image_processor/main.py
+++ src/hourly_image_processor/main.py
@@ -1,11 +1,16 @@
 from hourly_image_processor.daylight import is_light_enough
+from hourly_image_processor.camera import take_picture
+
 def main():
     if not is_light_enough():
         return
 
     image = take_picture()
-    image = process_image(image)
-    upload_image(image)
+
+    print(f'Image created: {image}')
+    
+    # image = process_image(image)
+    # upload_image(image)
 
 
 if __name__ == "__main__":
