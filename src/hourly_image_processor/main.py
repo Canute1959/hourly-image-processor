e7fc8d9ba6ebdd01ce7e7591219c154a5a1aef2b
Knut Alseth <knut@alseth.no>
2026-09-26 10:25:06 +0200

    start med config

--- src/hourly_image_processor/main.py
+++ src/hourly_image_processor/main.py
@@ -1,9 +1,12 @@
 from hourly_image_processor.daylight import is_light_enough
 from hourly_image_processor.camera import take_picture
 from hourly_image_processor.image_processor import process_image
+# from hourly_image_processor.config import load_config
 # from hourly_image_processor.uploader import upload_image
 
 def main():
+    # config = load_config()
+
     if not is_light_enough():
         return
 
